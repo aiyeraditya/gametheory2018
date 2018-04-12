377286d545b185ebb2619a4bbf148d86e20bf738
pranshugaba <pranshugaba@gmail.com>
2018-04-12 19:35:29 +0530

    Fixed edge widths and edge width labels

--- visual_graph.py
+++ visual_graph.py
@@ -11,23 +11,23 @@ def draw_graph(node_list, weights, name, saveas):
         G.add_node(node)
     pos=nx.circular_layout(G)
 
-    node_colors = ['red' if node == 1 else 'green' for node in node_type]
+    node_colors = ['#ff00ff' if node == 1 else '#00b8f8' for node in node_type]
     nx.draw_networkx_nodes(G,pos,node_color=node_colors, node_size=750)
     for i in range(len(node_list)):
         for j in range(i+1, len((node_list))):
-            G.add_edge(node_list[i], node_list[j], weight = weights[i][j])      # Define the weights
+            G.add_edge(node_list[i], node_list[j], weight = weights[i][j])  # Define the weights
 
     # Draw Graph with widths corresponding to weights
     for i in range(len(node_list)):
         for j in range(len(node_list)):
-            nx.draw_networkx_edges(G, pos, edgelist = [(node_list[i],node_list[j])], width = weights[i][j]/15);
+            nx.draw_networkx_edges(G, pos, edgelist = [(node_list[i],node_list[j])], width = weights[i][j]);
                 # Draws the Graph edge wise with width corresponding to weights
 
     arc_weight = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=arc_weight, font_color='black', label_pos=0.3)
     plt.axis('off')
-    plt.title(name);
-    plt.savefig(saveas + ".png")
+    #plt.title(name);
+    plt.savefig(saveas + ".pdf")
     #plt.show()
 
 # Birth death update rule
@@ -56,6 +56,7 @@ def birth_death(node_list, weights, cycle):
 
     for i in range(len(weights)):
         weights[i][node_to_die] = weights[i][node_to_reproduce]
+        weights[node_to_die][i] = weights[node_to_reproduce][i]
 
     weights[node_to_die][node_to_reproduce] = 1
     weights[node_to_reproduce][node_to_die] = 1
@@ -108,23 +109,28 @@ def evolve(node_list, weights, cycle):
         print("The node types are", node_type)
         print("The total payoffs are", payoff)
         print("The transformed payoffs are", transformed_payoff, "\n")
-
+        print("The weights are", weights, "\n")
+        plt.close()
         draw_graph(node_list, weights, "Stage %d" % i, "stage %d" % i)
-        death_birth(node_list, weights, cycle)
+        birth_death(node_list, weights, cycle)
         print("Step", i + 1, "completed\n")
 
-    # Rule for Evolution Here
 
+    # Rule for Evolution Here
 node_list = [0, 1, 2, 3, 4]
 
 # node_type tells whether the node is C (0) or D (1)
-node_type = [0, 0, 1, 1, 0]
+node_type = [0, 0, 0, 0, 1]
 
 # Payoff matrix for two players
 payoff_matrix = [[3, 1], [4, 2]]
 
-weights = [[0, 1, 2, 3, 4], [1, 0, 3, 0, 4], [2,3,0, 1, 1], [1, 4, 0, 0, 0], [4, 3, 2, 1, 0]]
-
+weights = [ [0, 1, 2, 1, 4],
+            [1, 0, 3, 4, 3],
+            [2, 3, 0, 0, 2],
+            [1, 4, 0, 0, 1],
+            [4, 3, 2, 1, 0]]
+#weights = [[1 for i in range(5)] for i in range(5)]
 #instantiating the payoff matrix f_j
 payoff = [0, 0, 0, 0, 0]
 # F_j
@@ -145,6 +151,6 @@ def update_payoffs(node_list, weights):
         transformed_payoff[i] =  transform(payoff[i])
 
 update_payoffs(node_list, weights)
-cycle = 50
+cycle = 20
 evolve(node_list, weights, cycle)
 #draw_graph(node_list, weights, "Title", "Saveas")
